6565bf10280af8e9f7818e6ca1bbd90b301c86cd
odednoam <oded@swagless.shop>
2020-07-11 23:18:34 +0300

    feat(admin-ui): Added duplicate product option in admin-ui

--- packages/admin-ui/src/lib/catalog/src/components/product-list/product-list.component.ts
+++ packages/admin-ui/src/lib/catalog/src/components/product-list/product-list.component.ts
@@ -3,19 +3,24 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { marker as _ } from '@biesbjerg/ngx-translate-extract-marker';
 import {
     BaseListComponent,
+    CreateProductVariantInput,
     DataService,
     JobQueueService,
     JobState,
+    LanguageCode,
     LogicalOperator,
     ModalService,
     NotificationService,
+    Product,
+    ProductWithVariantsFragment,
     SearchInput,
     SearchProducts,
 } from '@vendure/admin-ui/core';
 import { EMPTY, Observable } from 'rxjs';
 import { delay, map, switchMap, take, takeUntil, withLatestFrom } from 'rxjs/operators';
 
-import { ProductSearchInputComponent } from '../product-search-input/product-search-input.component';
+import { ProductSearchInputComponent } from '../../../../../../package/catalog';
+import { CreateProductInput } from '../../../../core/src/common/generated-types';
 
 @Component({
     selector: 'vdr-products-list',
@@ -43,7 +48,7 @@ export class ProductListComponent
         super.setQueryFn(
             (...args: any[]) =>
                 this.dataService.product.searchProducts(this.searchTerm, ...args).refetchOnChannelChange(),
-            data => data.search,
+            (data) => data.search,
             // tslint:disable-next-line:no-shadowed-variable
             (skip, take) => ({
                 input: {
@@ -60,20 +65,20 @@ export class ProductListComponent
 
     ngOnInit() {
         super.ngOnInit();
-        this.facetValues$ = this.result$.pipe(map(data => data.search.facetValues));
+        this.facetValues$ = this.result$.pipe(map((data) => data.search.facetValues));
         // this.facetValues$ = of([]);
         this.route.queryParamMap
             .pipe(
-                map(qpm => qpm.get('q')),
+                map((qpm) => qpm.get('q')),
                 takeUntil(this.destroy$),
             )
-            .subscribe(term => {
+            .subscribe((term) => {
                 this.productSearchInput.setSearchTerm(term);
             });
 
-        const fvids$ = this.route.queryParamMap.pipe(map(qpm => qpm.getAll('fvids')));
+        const fvids$ = this.route.queryParamMap.pipe(map((qpm) => qpm.getAll('fvids')));
 
-        fvids$.pipe(takeUntil(this.destroy$)).subscribe(ids => {
+        fvids$.pipe(takeUntil(this.destroy$)).subscribe((ids) => {
             this.productSearchInput.setFacetValues(ids);
         });
 
@@ -97,7 +102,7 @@ export class ProductListComponent
     rebuildSearchIndex() {
         this.dataService.product.reindex().subscribe(({ reindex }) => {
             this.notificationService.info(_('catalog.reindexing'));
-            this.jobQueueService.addJob(reindex.id, job => {
+            this.jobQueueService.addJob(reindex.id, (job) => {
                 if (job.state === JobState.COMPLETED) {
                     const time = new Intl.NumberFormat().format(job.duration || 0);
                     this.notificationService.success(_('catalog.reindex-successful'), {
@@ -112,6 +117,108 @@ export class ProductListComponent
         });
     }
 
+    async duplicateProduct(productId: string) {
+        function duplicateTranslation(
+            ti: any,
+            defaultLanguageCode: LanguageCode,
+            additionalFields?: string[],
+        ) {
+            if (Array.isArray(ti)) {
+                return ti.map((e) => duplicateTranslation(e, defaultLanguageCode, additionalFields));
+            }
+            const ret = {
+                languageCode: ti.languageCode == null ? defaultLanguageCode : ti.languageCode,
+                name: ti.name,
+            };
+            additionalFields?.forEach((f) => (ret[f] = ti[f]));
+            return ret;
+        }
+        function idArray(arr: any[]) {
+            return arr?.map((e) => e.id);
+        }
+
+        const productQuery = await this.dataService.product.getProduct(productId).single$.toPromise();
+        const oldProduct = productQuery.product;
+        if (oldProduct == null) {
+            return;
+        }
+        const pi: CreateProductInput = {
+            featuredAssetId: oldProduct.featuredAsset?.id,
+            assetIds: idArray(oldProduct.assets),
+            facetValueIds: idArray(oldProduct.facetValues),
+            translations: duplicateTranslation(oldProduct.translations, oldProduct.languageCode, [
+                'slug',
+                'description',
+            ]),
+        };
+        const createProductResult = await this.dataService.product.createProduct(pi).toPromise();
+        const newProduct: ProductWithVariantsFragment = createProductResult.createProduct;
+        const dataService = this.dataService;
+        async function duplicateProductOptionGroup(productVariants) {
+            const productOptionGroupIds = {};
+            const productOptionIds = {};
+            productVariants.forEach((v) => {
+                v.options.forEach((o) => {
+                    productOptionGroupIds[o.groupId] = true;
+                });
+            });
+            await Promise.all(
+                Object.keys(productOptionGroupIds).map(async (groupId) => {
+                    const pog = await dataService.product.getProductOptionGroup(groupId).single$.toPromise();
+                    if (pog.productOptionGroup == null) {
+                        return;
+                    }
+                    const oldOptionByCode = {};
+                    pog.productOptionGroup.options.forEach((o) => (oldOptionByCode[o.code] = o.id));
+                    const mut = await dataService.product
+                        .createProductOptionGroups({
+                            code: pog.productOptionGroup.code,
+                            translations: duplicateTranslation(
+                                pog.productOptionGroup.translations,
+                                pog.productOptionGroup.languageCode,
+                            ),
+                            options: pog.productOptionGroup.options.map((opt) => {
+                                return {
+                                    code: opt.code,
+                                    translations: duplicateTranslation(opt.translations, opt.languageCode),
+                                };
+                            }),
+                        })
+                        .toPromise();
+                    productOptionGroupIds[groupId] = mut.createProductOptionGroup.id;
+                    mut.createProductOptionGroup.options.forEach((o) => {
+                        productOptionIds[oldOptionByCode[o.code]] = o.id;
+                    });
+                    return await dataService.product
+                        .addOptionGroupToProduct({
+                            productId: newProduct.id,
+                            optionGroupId: mut.createProductOptionGroup.id,
+                        })
+                        .toPromise();
+                }),
+            );
+            return productOptionIds;
+        }
+        const optionIdMapping = await duplicateProductOptionGroup(oldProduct.variants);
+        const newVariants: CreateProductVariantInput[] = oldProduct.variants.map((v) => {
+            return {
+                productId: newProduct.id,
+                translations: duplicateTranslation(v.translations, v.languageCode),
+                facetValueIds: idArray(v.facetValues),
+                sku: v.sku,
+                price: v.price,
+                taxCategoryId: v.taxCategory?.id,
+                optionIds: v.options.map((o) => optionIdMapping[o.id]),
+                featuredAssetId: v.featuredAsset?.id,
+                assetIds: idArray(v.assets),
+            };
+        });
+        const createProductVariantsResult = await this.dataService.product
+            .createProductVariants(newVariants)
+            .toPromise();
+        return await this.router.navigate(['catalog', 'products', newProduct.id]);
+    }
+
     deleteProduct(productId: string) {
         this.modalService
             .dialog({
@@ -122,7 +229,9 @@ export class ProductListComponent
                 ],
             })
             .pipe(
-                switchMap(response => (response ? this.dataService.product.deleteProduct(productId) : EMPTY)),
+                switchMap((response) =>
+                    response ? this.dataService.product.deleteProduct(productId) : EMPTY,
+                ),
             )
             .subscribe(
                 () => {
@@ -131,7 +240,7 @@ export class ProductListComponent
                     });
                     this.refresh();
                 },
-                err => {
+                (err) => {
                     this.notificationService.error(_('common.notify-delete-error'), {
                         entity: 'Product',
                     });
